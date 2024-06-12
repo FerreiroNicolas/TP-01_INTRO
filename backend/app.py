cf57715d794f1088615a81c18b947309ad5dbd1a
JOACOeze2004 <jmendeza@fi.uba.ar>
2024-06-12 19:51:47 -0300

    Agrego funcionalidad del catalogo, junto con la modificacion del app.py y del api.js. Ademas agrego una carpeta Backup Data Bases, para que vayamos poniendo backups de las bases de datos

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,67 @@
-from flask import Flask, render_template
+import sys
+from flask import Flask,jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+app = Flask(__name__)
+port = 5000
 
-app = Flask(__name__,template_folder='../frontend')
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://joacoeze:joaquinm@localhost:5432/catalogo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+db = SQLAlchemy(app)
+
 CORS(app)
 
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+
+class catalogo(db.Model):
+    __tablename__ = 'catalogo'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_de_pelicula = db.Column(db.String(100), nullable=False)
+    url_imagen = db.Column(db.String(400))
+    año_de_estreno = db.Column(db.Integer)
+    genero = db.Column(db.String(100))
+    duracion = db.Column(db.Integer)
+    sinopsis = db.Column(db.String(1000))
+    director = db.Column(db.String(200))
+    actores_principales = db.Column(db.String(300))
+    productora = db.Column(db.String(100))
+    pais_de_origen = db.Column(db.String(100))
+    puntaje_segun_critica = db.Column(db.Integer)
+    url_trailer = db.Column(db.Text)
 
 
+@app.route("/api.html", methods=['GET'])
+def obtener_peliculas():
+    try:
+        peliculas = catalogo.query.order_by(catalogo.nombre_de_pelicula).all()
+        peliculas_json = []
+        for pelicula in peliculas:
+            pelicula_dict = {
+                'id': pelicula.id,
+                'nombre_de_pelicula': pelicula.nombre_de_pelicula,
+                'url_imagen': pelicula.url_imagen,
+                'año_de_estreno': pelicula.año_de_estreno,
+                'genero': pelicula.genero,
+                'duracion': pelicula.duracion,
+                'sinopsis': pelicula.sinopsis,
+                'director': pelicula.director,
+                'actores_principales': pelicula.actores_principales,
+                'productora': pelicula.productora,
+                'pais_de_origen': pelicula.pais_de_origen,
+                'puntaje_segun_critica': pelicula.puntaje_segun_critica,
+                'url_trailer': pelicula.url_trailer
+            }
+            peliculas_json.append(pelicula_dict)
+        return jsonify({'peliculas': peliculas_json})
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
 
+
+if __name__ == '__main__':
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', debug=True, port=port)
--- frontend/statics/js/api.js
+++ frontend/statics/js/api.js
@@ -1,23 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const movies = localStorage.getItem('movies');
-    if (movies) {
-        const parsedMovies = JSON.parse(movies);
-        let output = '';
-        parsedMovies.forEach(movie => {
-            output += `
-                <div class="col-md-3 mb-4">
-                    <div class="card">
-                        <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title}">
+function response_recibed(response) {
+    return response.json();
+}
+    
+function data(data) {
+    const movies = data.peliculas;
+    let output = '';
+    movies.forEach(movie => {
+        output += `
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img src="${movie.url_imagen}" class="card-img-top" alt="${movie.nombre_de_pelicula}">
                         <div class="card-body">
-                            <h5 class="card-title">${movie.Title}</h5>
-                            <p class="card-text">Año: ${movie.Year}</p>
+                            <h5 class="card-title">${movie.nombre_de_pelicula}</h5>
+                            <p class="card-text">Año: ${movie.año_de_estreno}</p>
                         </div>
-                    </div>
                 </div>
+            </div>
             `;
         });
         document.getElementById('api-movies').innerHTML = output;
-    } else {
-        document.getElementById('api-movies').innerHTML = '<p>No se encontraron resultados.</p>';
     }
-});
+
+function request_error(error) {
+    alert("ERROR: " + error);
+    console.error('Error al recuperar datos de películas:', error);
+    document.getElementById('api-movies').innerHTML = '<p>No se pudieron cargar los datos de películas.</p>';
+}
+
+fetch('http://localhost:5000/api.html')
+    .then(response_recibed)
+    .then(data)
+    .catch(request_error);
+
+
